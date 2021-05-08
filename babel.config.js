41b5656762268ee45f57863985af344d4beac7a8
Emmanuel Antonio <developer.ocansey@gmail.com>
2021-05-08 16:27:35 +0300

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+{
+  plugins: [
+    [
+       'module-resolver',
+       {
+         root: ['./src'],
+         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+         alias: {
+           tests: ['./tests/'],
+           "@components": "./src/components",
+         }
+       }
+    ]
+  ]
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import App from './App';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
